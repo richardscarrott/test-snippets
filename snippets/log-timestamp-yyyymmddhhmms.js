e6583d172a082f863c0125b48deecee5770db3a9
Richard Scarrott <riscarrott@googlemail.com>
2018-08-24 10:31:11 +0100

    Fix sequelize timestamp snippet

--- snippets/log-timestamp-yyyymmddhhmms.js
+++ snippets/log-timestamp-yyyymmddhhmms.js
@@ -1,5 +1,6 @@
 // https://github.com/sequelize/cli/blob/master/src/helpers/path-helper.js#L13
 // (because sequelize-cli doesn't support typescript)
+const format = i => (parseInt(i, 10) < 10 ? "0" + i : i);
 const date = new Date();
 console.log(
   [
