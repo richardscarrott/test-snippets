bf54560b86d8c79b9e7d7eee728969d9f73abd85
Richard Scarrott <riscarrott@googlemail.com>
2019-06-24 12:16:48 +0100

    Create gitlab-ci-variable-fix.js

---
+++ snippets/gitlab-ci-variable-fix.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.ci-variable-body-item').forEach((el) => {
+  el.style.maxWidth = 'none';
+});
+document.querySelectorAll('.js-ci-variable-input-value').forEach((el) => {
+  el.style.width = '600px';
+});
