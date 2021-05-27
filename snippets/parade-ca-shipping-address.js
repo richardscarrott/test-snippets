022e3ccc8eab76091ae9443e7ca5fa8c0b41f490
Richard Scarrott <riscarrott@googlemail.com>
2021-05-27 14:11:14 +0100

    Update parade-ca-shipping-address.js

--- snippets/parade-ca-shipping-address.js
+++ snippets/parade-ca-shipping-address.js
@@ -2,7 +2,12 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 // https://github.com/facebook/react/issues/11488#issuecomment-347775628
-const setValue = (input, value) => { 
+const setValue = (selector, value) => { 
+  const input = $(selector);
+  if (!input) {
+    console.warn(`Input not found: ${selector}`);
+    return;
+  }
   let lastValue = input.value;
   input.value = value;
   let event = new Event('input', { bubbles: true });
@@ -16,11 +21,11 @@ const setValue = (input, value) => {
   input.dispatchEvent(event);
 }
 
-setValue($('input[name=email]'), 'richard.scarrott@paradeworld.co');
-setValue($('input[name=shippingAddress\\.firstName]'), 'Shippy');
-setValue($('input[name=shippingAddress\\.lastName]'), 'Shippington');
-setValue($('input[name=shippingAddress\\.line1]'), '4026 Munsee Street');
-setValue($('input[name=shippingAddress\\.line2]'), '');
-setValue($('input[name=shippingAddress\\.city]'), 'Cayuga');
-setValue($('input[name=shippingAddress\\.postCode]'), 'N0A 1E0');
-setValue($('input[name=shippingAddress\\.phone]'), '905-772-2386');
+setValue('input[name=email]', 'richard.scarrott@paradeworld.co');
+setValue('input[name=shippingAddress\\.firstName]', 'Shippy');
+setValue('input[name=shippingAddress\\.lastName]', 'Shippington');
+setValue('input[name=shippingAddress\\.line1]', '4026 Munsee Street');
+setValue('input[name=shippingAddress\\.line2]', '');
+setValue('input[name=shippingAddress\\.city]', 'Cayuga');
+setValue('input[name=shippingAddress\\.postCode]', 'N0A 1E0');
+setValue('input[name=shippingAddress\\.phone]', '905-772-2386');
